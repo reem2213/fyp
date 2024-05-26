3a11640cf4af3eff0ad7c91f23fb20b13f501629
Reem <202110769@ua.edu.lb>
2024-05-27 01:09:33 +0300

    update sign up and profile UI

--- client/App.js
+++ client/App.js
@@ -29,7 +29,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="AuthenticationTester" headerMode="none">
+      <Stack.Navigator initialRouteName="Customize" headerMode="none">
         <Stack.Screen
           name="Home"
           component={Home}
